9ab2878fe054529b148d2b4e21fbe1dbdba525ce
Ogbonna Blessed <blessedlyrics11@gmail.com>
2026-04-15 10:34:53 +0100

    fix: fix testimony carousel

--- src/components/shared/Carousel.tsx
+++ src/components/shared/Carousel.tsx
@@ -69,17 +69,17 @@ const CarouselDemo = () => {
           {testimonies.map((t, index) => (
             <CarouselItem key={index} className="basis-full lg:basis-1/2">
               <Card className="w-full max-w-full flex flex-col items-start p-8 md:p-8! border-0 bg-[#0A42D0]/10">
-                <div className="relative w-14 sm:w-16 md:w-16 aspect-square rounded-full overflow-hidden">
+                <div className="relative w-16 md:w-16 aspect-square rounded-full overflow-hidden">
                   <img
                     src={t.image}
                     alt={t.name}
                     className="object-cover object-top"
                   />
                 </div>
-                <p className="text-muted-foreground text-sm sm:text-base w-full">
+                <p className="text-muted-foreground text-base w-full">
                   {t.about}
                 </p>
-                <h3 className="text-base md:text-lg mt-5">
+                <h3 className="text-lg mt-5">
                   {t.name}
                 </h3>
               </Card>
